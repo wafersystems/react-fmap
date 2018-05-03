aabcce7ab3f882ffd0088e9babae1e0b5c936ab9
kairi1227 <kairi1227@126.com>
2018-05-03 17:32:53 +0800

    add text mark function

--- src/App.js
+++ src/App.js
@@ -9,10 +9,34 @@ class App extends Component {
 
 		const mapProps = {
 			fmapID: 'wafer-beijing',
-			appName:'北京威发新世纪',
-			mapKey:'3e7dba2418ac46e1af5a1abf17082db2',
-			height: 'calc(100vh - 254px)'
+			appName: '北京威发新世纪',
+			mapKey: '3e7dba2418ac46e1af5a1abf17082db2',
+			height: 'calc(100vh - 254px)',
+			defaultViewMode: 'top'
 		};
+
+		const stations = [
+			{
+				x: 12958821.565,
+				y: 4852547.234999999,
+				z: 0,
+				name: '3F01'
+			},
+			{
+				id: 123,
+				stationId: 111,
+				x: 12958815.715,
+				y: 4852542.055,
+				z: 0,
+				name: '3F02'
+			},
+			{
+				x: 12958819.295,
+				y: 4852550.31,
+				z: 0,
+				name: '3F03'
+			}
+		];
 
 		return (
 			<div className="App">
@@ -23,7 +47,7 @@ class App extends Component {
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
-				<FMap {...mapProps}/>
+				<FMap {...mapProps} textMarkers={stations} onClick={e => console.log(e)}/>
 			</div>
 		);
 	}
--- src/FMap.js
+++ src/FMap.js
@@ -2,25 +2,56 @@
  * date: 2018-05-03 13:52
  * auth: XuQiang
  **/
-import React, {PureComponent} from 'react';
+import React, {Component} from 'react';
 import PropsTypes from 'prop-types';
 
-class FMap extends PureComponent {
+class FMap extends Component {
+
+	map = null;
+	fengmap = window.fengmap;
 
 	componentDidMount() {
-		const {fmapID, appName, mapKey, onClick} = this.props;
-		const map = new window.fengmap.FMMap({
+		const {fmapID, appName, mapKey, onClick, fMMapProps, defaultMapScaleLevel, defaultViewMode, textMarkers} = this.props;
+		this.map = new this.fengmap.FMMap({
 			container: document.getElementById('fmap-container'), //渲染dom
 			appName,           //开发者应用名称
-			key: mapKey   //开发者申请应用下web服务的key
+			key: mapKey,   //开发者申请应用下web服务的key
+			defaultMapScaleLevel,
+			defaultViewMode,
+			...fMMapProps
 		});
 		//打开Fengmap服务器的地图数据和主题
-		map.openMapById(fmapID);
+		this.map.openMapById(fmapID);
 
-		map.on('mapClickNode', (event) => {
+		this.map.on('mapClickNode', (event) => {
 			onClick(event);
+			if(event.nodeType === 5) {
+			}
 		});
+
+		this.map.on('loadComplete', () => {
+			this.addTextMarker(textMarkers);
+		});
+
 	}
+
+	addTextMarker(textMarkers) {
+		const group = this.map.getFMGroup(this.map.groupIDs[0]);
+		const layer = new this.fengmap.FMTextMarkerLayer();
+		group.addLayer(layer);
+		for(const mark of textMarkers) {
+			const im = new this.fengmap.FMTextMarker({
+				...mark,
+				x: mark.x,
+				y: mark.y,
+				name: mark.name || '',
+				fontsize: mark.fontSize || 12, //字体大小
+				callback: () => im.alwaysShow()
+			});
+			layer.addMarker(im);
+		}
+	}
+
 
 	render() {
 		const {className, width, height} = this.props;
@@ -43,7 +74,11 @@ FMap.propTypes = {
 	onClick: PropsTypes.func,
 	className: PropsTypes.string,
 	width: PropsTypes.string,
-	height: PropsTypes.string
+	height: PropsTypes.string,
+	defaultViewMode: PropsTypes.oneOf(['3d', 'top']),
+	fMMapProps: PropsTypes.object,
+	defaultMapScaleLevel: PropsTypes.number,
+	textMarkers: PropsTypes.array
 };
 
 FMap.defaultProps = {
@@ -52,7 +87,11 @@ FMap.defaultProps = {
 	},
 	className: undefined,
 	width: '100%',
-	height: '100%'
+	height: '100%',
+	defaultViewMode: window.fengmap.FMViewMode.MODE_2D,
+	fMMapProps: {},
+	defaultMapScaleLevel: undefined,
+	textMarkers: []//{name, x, y, fontSize, }
 };
 
 export default FMap;
