d4ab4f3d1f2c8c4535a673cf4e46bae9ff5bb4f0
wangxiao <wangxiao@wafersystems.com>
2019-05-02 23:16:08 +0800

    v.0.5.0 Increase the window.devicePixelRatio = 1 setting to solve the problem of mobile rendering deadlock.

--- src/App.js
+++ src/App.js
@@ -1,173 +1,227 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FMap from './FMap';
 
 class App extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      showMap: true,
+      stations: [
+        // {
+        // 	x: 12958821.565,
+        // 	y: 4852547.234999999,
+        // 	z: 0,
+        // 	name: '3F01'
+        // },
+        {
+          id: 123,
+          stationId: 111,
+          x: 12958815.715,
+          y: 4852542.055,
+          z: 0,
+          name: '3F02',
+        },
+        {
+          x: 12958819.295,
+          y: 4852550.31,
+          z: 0,
+          name: '3F03',
+        },
+      ],
+      popMarker: [],
+      images: [
+        {
+          x: 12958821.565,
+          y: 4852547.234999999,
+          z: 0,
+          name: '3F01',
+          url: '/static/media/logo.5d5d9eef.svg',
+        },
+      ],
+      rotate: 30,
+    };
+  }
 
-	constructor() {
-		super(...arguments);
-		this.state = {
-			showMap: true,
-			 stations : [
-				// {
-				// 	x: 12958821.565,
-				// 	y: 4852547.234999999,
-				// 	z: 0,
-				// 	name: '3F01'
-				// },
-				{
-					id: 123,
-					stationId: 111,
-					x: 12958815.715,
-					y: 4852542.055,
-					z: 0,
-					name: '3F02'
-				},
-				{
-					x: 12958819.295,
-					y: 4852550.31,
-					z: 0,
-					name: '3F03'
-				}
-			],
-			popMarker: [],
-			images : [
-				{
-					x: 12958821.565,
-					y: 4852547.234999999,
-					z: 0,
-					name: '3F01',
-					url: '/static/media/logo.5d5d9eef.svg'
-				}
-			],
-			rotate: 30
-		};
-	}
+  render() {
+    const mapProps = {
+      fmapID: 'wafer-beijing',
+      appName: '北京威发新世纪',
+      mapKey: '3e7dba2418ac46e1af5a1abf17082db2',
+      height: 'calc(100vh - 254px)',
+      defaultViewMode: 'top',
+      url: './fengmap.min.js',
+    };
 
-	render() {
+    const initialPosition = { x: 12958819.3, y: 4852556.59, z: 0 };
 
-		const mapProps = {
-			fmapID: 'wafer-beijing',
-			appName: '北京威发新世纪',
-			mapKey: '3e7dba2418ac46e1af5a1abf17082db2',
-			height: 'calc(100vh - 254px)',
-			defaultViewMode: 'top',
-			url: './fengmap.min.js'
-		};
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="App-intro">
+          <button
+            onClick={() => {
+              this.setState({ showMap: !this.state.showMap });
+            }}
+          >
+            map show or hidden
+          </button>
+          <button
+            onClick={() => {
+              this.map.setViewMode('3d');
+            }}
+          >
+            test view model
+          </button>
+          <button
+            onClick={() => {
+              this.map.getSearchReq({ ID: 61 }, v => {
+                console.log(v);
+                // this.map.addTextMarker([{
+                // 	x: 12958819.3,
+                // 	y: 4852556.59,
+                // 	z: 0,
+                // 	name: '3F04'
+                // }], v.groupID);
+                this.map.addImageMarker(
+                  [
+                    {
+                      x: 12958819.3,
+                      y: 4852556.59,
+                      z: 0,
+                      name: '3F01',
+                      url: '/static/media/logo.5d5d9eef.svg',
+                    },
+                  ],
+                  v.groupID
+                );
+              });
+            }}
+          >
+            search
+          </button>
+          <button
+            onClick={() => {
+              const map = this.map.onNavigation({});
+              console.log(map);
+            }}
+          >
+            navigation
+          </button>
+          <button
+            onClick={() => {
+              this.map.onMapFunction('moveTo', { x: 12958819.3, y: 4852556.59, groupID: 1, z: 0 });
+            }}
+          >
+            function call back
+          </button>
+          <button
+            onClick={() => {
+              this.setState({
+                stations: this.state.stations.concat({
+                  x: 12958819.3,
+                  y: 4852556.59,
+                  name: '3F05',
+                }),
+              });
+            }}
+          >
+            add text marker
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ stations: [] });
+            }}
+          >
+            remove text marker
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ images: [] });
+            }}
+          >
+            remove image marker
+          </button>
+          <button
+            onClick={() => {
+              this.setState({
+                images: this.state.images.concat([
+                  {
+                    x: 12958819.3,
+                    y: 4852556.59,
+                    z: 0,
+                    name: '3F01',
+                    url: '/static/media/logo.5d5d9eef.svg',
+                  },
+                ]),
+              });
+            }}
+          >
+            add image marker
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ rotate: 78 });
+            }}
+          >
+            set rotate
+          </button>
+        </p>
 
-		const initialPosition = {x: 12958819.3, y: 4852556.59, z: 0};
+        {this.state.showMap && (
+          <FMap
+            rotate={this.state.rotate}
+            {...mapProps}
+            imageMarkers={this.state.images}
+            loadComplete={() => {
+              this.test1();
+              this.test2();
+            }}
+            initialPosition={initialPosition}
+            toolControl={{ groupsButtonNeeded: true }}
+            textMarkers={this.state.stations}
+            onClick={e => {
+              console.log(e);
+              const station = {
+                mapCoord: {
+                  // 设置弹框的x轴
+                  x: e.x,
+                  // 设置弹框的y轴
+                  y: e.y,
+                  // 设置弹框位于的楼层
+                  groupID: e.groupID,
+                },
+                // 设置弹框的宽度
+                width: 200,
+                // 设置弹框的高度
+                height: 100,
+                marginTop: 10,
+                // 设置弹框的内容
+                content: '这是一个信息框',
+              };
+              this.map.setPopMarker(station);
+            }}
+            ref={r => (this.map = r)}
+          />
+        )}
+      </div>
+    );
+  }
 
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo"/>
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-				<p className="App-intro">
-					<button onClick={() => {
-						this.setState({showMap: !this.state.showMap});
-					}}>map show or hidden</button>
-					<button onClick={() => {
-						this.map.setViewMode('3d')
-					}}>test view model</button>
-					<button onClick={() => {
-						this.map.getSearchReq({ID: 61}, v => {
-							console.log(v)
-							// this.map.addTextMarker([{
-							// 	x: 12958819.3,
-							// 	y: 4852556.59,
-							// 	z: 0,
-							// 	name: '3F04'
-							// }], v.groupID);
-							this.map.addImageMarker([{
-								x:  12958819.3,
-								y: 4852556.59,
-								z: 0,
-								name: '3F01',
-								url: '/static/media/logo.5d5d9eef.svg'
-							}], v.groupID);
-						});
-					}}>
-						search
-					</button>
-					<button onClick={() => {
-						const map = this.map.onNavigation({});
-						console.log(map);
-					}}>
-						navigation
-					</button>
-					<button onClick={() => {
-						this.map.onMapFunction('moveTo', {x: 12958819.3, y: 4852556.59, groupID: 1, z: 0});
-					}}>
-						function call back
-					</button>
-					<button onClick={() => {
-						this.setState({stations: this.state.stations.concat({x:  12958819.3,
-								y: 4852556.59, name: '3F05'})})
-					}}>
-						add text marker
-					</button>
-					<button onClick={() => {
-						this.setState({stations: []})}}>
-						remove text marker
-					</button>
-					<button onClick={() => {
-						this.setState({images: []})}}>
-						remove image marker
-					</button>
-					<button onClick={() => {
-						this.setState({images: this.state.images.concat([{
-								x:  12958819.3,
-								y: 4852556.59,
-								z: 0,
-								name: '3F01',
-								url: '/static/media/logo.5d5d9eef.svg'
-							}])})}}>
-						add image marker
-					</button>
-					<button onClick={() => {
-							this.setState({rotate: 78})
-					}}>
-						set rotate
-					</button>
-				</p>
+  test1() {
+    console.log('test1');
+  }
 
-				{this.state.showMap && <FMap rotate={this.state.rotate} {...mapProps} imageMarkers={this.state.images} loadComplete={() => {
-					this.test1();
-					this.test2();
-				}} initialPosition={initialPosition} toolControl={{groupsButtonNeeded: true}} textMarkers={this.state.stations} onClick={e => {
-					console.log(e)
-					const station = {mapCoord: {
-							//设置弹框的x轴
-							x: e.x,
-							//设置弹框的y轴
-							y: e.y,
-							//设置弹框位于的楼层
-							groupID: e.groupID
-						},
-						//设置弹框的宽度
-						width: 200,
-						//设置弹框的高度
-						height: 100,
-						marginTop: 10,
-						//设置弹框的内容
-						content: '这是一个信息框'};
-					this.map.setPopMarker(station);
-				}} ref={r => this.map = r} />}
-			</div>
-		);
-	}
-
-	test1() {
-		console.log('test1');
-	}
-	test2() {
-		console.log('test2');
-	}
+  test2() {
+    console.log('test2');
+  }
 }
 
 export default App;
--- src/loadFengmap.js
+++ src/loadFengmap.js
@@ -1,30 +1,30 @@
 /**
  * date: 2018-05-07 14:24
  * auth: XuQiang
- **/
+ * */
 
-export const loadFengmap = (url) => {
-	return new Promise(resolve => {
-		if(window.fengmap || document.getElementById('fmap-script')) {
-			resolve(window.fengmap);
-		} else {
-			//得到html的头部dom
-			const theHead = document.getElementsByTagName('head').item(0);
-			//创建脚本的dom对象实例
-			let myScript = document.createElement('script');
-			myScript.id = 'fmap-script';
-			myScript.src = url;           //指定脚本路径
-			// myScript.defer = true;              //程序下载完后再解析和执行
-			theHead.appendChild(myScript); //把dom挂载到头部
-			let __js_interval = setInterval(() => {
-				if(window.fengmap) {
-					if(__js_interval) {
-						window.clearInterval(__js_interval);
-						__js_interval = 0;
-					}
-					resolve(window.fengmap);
-				}
-			}, 500);
-		}
-	});
+export const loadFengmap = url => {
+  return new Promise(resolve => {
+    if (window.fengmap || document.getElementById('fmap-script')) {
+      resolve(window.fengmap);
+    } else {
+      // 得到html的头部dom
+      const theHead = document.getElementsByTagName('head').item(0);
+      // 创建脚本的dom对象实例
+      const myScript = document.createElement('script');
+      myScript.id = 'fmap-script';
+      myScript.src = url; // 指定脚本路径
+      // myScript.defer = true;              //程序下载完后再解析和执行
+      theHead.appendChild(myScript); // 把dom挂载到头部
+      let __js_interval = setInterval(() => {
+        if (window.fengmap) {
+          if (__js_interval) {
+            window.clearInterval(__js_interval);
+            __js_interval = 0;
+          }
+          resolve(window.fengmap);
+        }
+      }, 500);
+    }
+  });
 };
